735c536389f359842016a27c5ee7f4ebd344f263
vikram <vikram.bedi97@gmail.com>
2018-03-19 23:10:47 +0530

    File read write

--- webserver.js
+++ webserver.js
@@ -1,14 +1,16 @@
 // Api
+let todos=[];
 
 const express=require('express');
 const path=require('path');
+const fs=require('fs');
 const app=express();
+const file=path.join(__dirname,"todo.json");
 
 app.listen(3333,function () {
     console.log("Server started on http://localhost:3333")
 });
 
-let todos=[];
 
 app.use('/',express.static(path.join(__dirname,'Static')));
 
@@ -17,7 +19,24 @@ app.get('/todo',function (request,response) {
 });
 
 app.get('/addTodo',function (request,response) {
-   todos.push(request.query.p);
+   //todos.push(request.query.p);
+   addTodos(request.query.p);
    response.send("Success");       // Mandatory to send response
 });
 
+function addTodos(task) {
+   todos.push(task);
+   let json=JSON.stringify("task",todos);
+   fs.writeFile(
+       file,
+       json,
+       function (err) {
+           if(err){
+               throw err;
+           }
+           else{
+               console.log("File written");
+           }
+       }
+   )
+}
